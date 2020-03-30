0fb4d57a946f0d3bde1d7c11fac342a14d8c556f
Amaterasus <skyetay03@googlemail.com>
2020-03-30 21:56:50 +0100

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def line(deli)
+    output = "The line is currently:"
+    if deli.any?
+        deli.each_with_index { |c, i| output += " #{i + 1}. #{c}"}
+    else
+        output = "The line is currently empty."
+    end
+    puts output
+end
+
+def take_a_number(deli, customer)
+    deli << customer
+    puts "Welcome, #{customer}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+    customer = deli.shift
+    output = customer ? "Currently serving #{customer}." : "There is nobody waiting to be served!"
+    puts output
+end
+
